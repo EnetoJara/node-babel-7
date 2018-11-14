e4e1b1558c9d4795092c6bf04d32b732603a1f9e
ernesto <ernesto.jara06@gmail.com>
2018-11-13 21:41:17 -0600

    starting the server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import express from 'express'
 import bodyParse from 'body-parser'
+import mongoose from 'mongoose'
+import passport from 'passport'
+import path from 'path'
 
 import indexRoute from './routes/indexRoutes'
+import users from './routes/users'
+import profile from './ṕrofiles'
+import posts from './routes/profiles'
+
+import { mongoURI } from './config/keys'
 
 const app = express()
+
+app.use(bodyParse.urlencoded({ extended: false }))
 app.use(bodyParse.json())
+
 
 app.use('/api', indexRoute)
 
